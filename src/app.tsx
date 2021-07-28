920a27a554aea6d8f5610634b0bce043ebd004b3
yqLiu2019 <54939945+yqLiu2019@users.noreply.github.com>
2021-07-28 20:01:36 +0800

    feat: init

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const App: React.FC = () => {
+  //  NOTE HERE:
+  //  hello react will appear twice
+  //  so this bundle has been executed twice
+  console.log('hello react')
+  // but this clicked never appears
+  return <button onClick = {() => {console.log('clicked')}}> Click here</button>
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+var config = {
+  entry: ['./src/index.tsx'],
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'ts-loader',
+      }
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+  ],
+}
+
+module.exports = config;
